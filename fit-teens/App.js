0989bba7d195bdee8c8ffa4d1d2b2bb73e037dfc
emalia <qori.emalia.21@student.if.ittelkom-sby.ac.id>
2023-12-22 00:55:34 +0700

    about us

--- fit-teens/App.js
+++ fit-teens/App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config"
+import { AboutScreen, ProfileScreen, TaskCompletedScreen, TaskScreen } from './screens';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const BottomNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Task') {
+            iconName = 'tasks';
+          } else if (route.name === 'Completed') {
+            iconName = 'clipboard-check';
+          } else if (route.name === 'About') {
+            iconName = 'exclamation-circle';
+          }
+          return (
+            <FontAwesome5
+              name={iconName}
+              size={size}
+              color={focused ? 'navy' : color}
+            />
+          );
+        },
+        tabBarIconStyle: { marginTop: 10 },
+        tabBarLabel: ({ children, color, focused }) => {
+          return (
+            <Text style={{ marginBottom: 10, color: focused ? 'navy' : color }}>
+              {children}
+            </Text>
+          );
+        },
+        tabBarStyle: {
+          height: 70,
+          borderTopWidth: 0,
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Task"
+        component={TaskScreen}
+        options={{ title: 'All Task', unmountOnBlur: true }}
+      />
+      <Tab.Screen
+        name="Completed"
+        component={TaskCompletedScreen}
+        options={{ unmountOnBlur: true }}
+      />
+      <Tab.Screen
+        name="About Us"
+        component={AboutScreen}
+        options={{ unmountOnBlur: true }}
+      />
+    </Tab.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <Provider store={store}>
+      <GluestackUIProvider config={config}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="BottomNavigator"
+              component={BottomNavigator}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Profile"
+              component={ProfileScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </GluestackUIProvider>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ fit-teens/screens/about.js
@@ -0,0 +1,119 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    Linking,
+    ScrollView
+} from "react-native";
+import { Button, ButtonText } from '@gluestack-ui/themed';
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+
+const About = () => {
+    const navigation = useNavigation();
+    const { nim, nama } = useSelector((state) => state.profile);
+
+    return (
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.aboutUs}>
+            <View style={styles.container}>
+                <Text style={styles.p}>
+                Aplikasi ini dirancang untuk memenuhi Ujian Akhir Semester Mata Kuliah Pemrograman Mobile Informatika Telkom University Surabaya
+                </Text>
+                <Text style={styles.textWrapper}>Get to know us:</Text>
+                <Image style={styles.ema} source={require('../assets/dina.png')} />
+                <Image style={styles.dina} source={require('../assets/ema.png')} />
+                <Image style={styles.erica} source={require('../assets/erica.png')} />
+                <Image style={styles.elma} source={require('../assets/elma.png')} />
+            </View>
+            </View>
+        </ScrollView>
+      );
+};
+
+const styles = StyleSheet.create({
+    aboutUs: {
+        backgroundColor: '#0f0f0f',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+    },
+    container: {
+        backgroundColor: 'rgba(15, 16, 16, 1)',
+        height: 1777,
+        width: 393,
+    },
+    p: {
+        color: '#ffffff',
+        fontFamily: 'Montserrat-Medium',
+        fontSize: 12,
+        fontWeight: '500',
+        left: 37,
+        letterSpacing: 0,
+        lineHeight: 16,
+        position: 'absolute',
+        textAlign: 'center',
+        top: 155,
+        width: 320,
+    },
+    textWrapper: {
+        color: '#ffffff',
+        fontFamily: 'Montserrat-Medium',
+        fontSize: 12,
+        fontWeight: '500',
+        left: 37,
+        letterSpacing: 0,
+        lineHeight: 16,
+        position: 'absolute',
+        textAlign: 'center',
+        top: 224,
+        width: 320,
+    },
+    dina: {
+        height: 297,
+        left: 52,
+        position: 'absolute',
+        top: 286,
+        width: 289,
+    },
+    ema: {
+        height: 308,
+        left: 23,
+        position: 'absolute',
+        top: 637,
+        width: 318,
+    },
+    erica: {
+        height: 302,
+        left: 52,
+        position: 'absolute',
+        top: 999,
+        width: 289,
+    },
+    elma: {
+        height: 304,
+        left: 52,
+        position: 'absolute',
+        top: 1356,
+        width: 289,
+    },
+    // title: {
+    //     fontSize: 24,
+    //     fontWeight: "bold",
+    //     marginBottom: 20,
+    // },
+    // heading: {
+    //     fontSize: 30,
+    //     fontWeight: "bold",
+    //     marginBottom: 7,
+    //     color: "blue",
+    // },
+    // content: {
+    //     fontSize: 18,
+    //     marginBottom: 20,
+    // }
+});
+
+export default About;
